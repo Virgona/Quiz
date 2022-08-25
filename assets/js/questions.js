75eeca997deb4934e46ba80d6ecfcbfc9910756f
Tobias Virgona <tobiasvirgona@gmail.com>
2022-08-25 16:52:18 +1000

    it doesnt look amzing. But it runs exactly how is should!

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -14,7 +14,7 @@ var questions = [
     {
         title: 'Arrays in JavaScript can be used to store ____.',
         choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
-        answer: 'all of the above;',
+        answer: 'all of the above',
     },
     {
         title: 'String values must be enclosed within ____ when being assigned to variables.',
@@ -24,6 +24,6 @@ var questions = [
     {
         title: 'A very useful tool used furing development and debugging for printing content to the debugger is:',
         choices: ['JavaScript', 'terminal / bash', 'for loops', 'console log'],
-        answer: 'console.log'
+        answer: 'console log'
     },
 ];
